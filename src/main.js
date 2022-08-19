921325831efee29827cc87be3c88ada9ec3523af
codedbychavez <codedbychavez@gmail.com>
2022-08-18 21:44:30 -0300

    Update

--- src/main.js
+++ src/main.js
@@ -13,8 +13,8 @@ app.use(createPinia())
 app.use(router)
 
 app.use(gapiPlugin, {
-  apiKey: 'AIzaSyAL_ZJq7uJPIKtifveUKXdEZXX4MS9W2b8',
-  clientId: '920632948783-ia29gokkbdl28l3u4rq3incesj27nhud.apps.googleusercontent.com',
+  apiKey: '<GOOGLE_PROJECT_API_KEY>',
+  clientId: '<CLIENT_ID>.apps.googleusercontent.com',
   discoveryDocs: ['https://mybusinessbusinessinformation.googleapis.com/$discovery/rest?version=v1'],
   scope: 'https://www.googleapis.com/auth/business.manage email'
 })
